12d3a7672bd20db50f2d90d3a64f94651695b5a6
Awez Mirza <awezbaigmirza@gmail.com>
2023-11-21 13:48:56 +0530

    Time: 159 ms (83.89%) | Memory: 102.4 MB (58.59%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
         int n = garbage.size();
-        int lastOccG = 0, lastOccP = 0, lastOccM = 0;
-        int ans = 0;
+        int lastOccG =0, lastOccP =0, lastOccM =0, ans = 0;
         for(int i = 0;i<n;i++){
             int j = 0;
-            while(j<garbage[i].size()){
-                if(garbage[i][j] == 'G'){
+            for(char c : garbage[i]){
+                if(c == 'G'){
                     ans += 1 + lastOccG;
                     lastOccG = 0;
                 }
-                else if(garbage[i][j] == 'P'){
+                else if(c == 'P'){
                     ans += 1 + lastOccP;
                     lastOccP = 0;
                 }
@@ -19,7 +18,6 @@ public:
                     ans += 1 + lastOccM;
                     lastOccM = 0;
                 }
-                j++;
             }
             if(i != n-1){
                 lastOccP += travel[i];
