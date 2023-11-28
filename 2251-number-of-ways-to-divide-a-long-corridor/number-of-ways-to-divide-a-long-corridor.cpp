3d7783dc7d5822cd936ca659fe6e1ed18282c95b
Awez Mirza <awezbaigmirza@gmail.com>
2023-11-28 13:29:53 +0530

    Time: 113 ms (90.86%) | Memory: 30.8 MB (100.00%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -3,7 +3,7 @@ public:
     int numberOfWays(string corridor) {
         int mod = 1000000007;
         int sze = corridor.size();
-        long ans = 0;
+        long ans = 1;
         int crrPlnts = 0;
         int s = 0;
         for(int i = 0;i<sze;i++){
@@ -13,16 +13,14 @@ public:
                     i++;
                 }
                 if(corridor[i]=='S'){
-                    if(ans == 0) ans = crrPlnts + 1;
-                    else ans = (ans*(crrPlnts + 1))%mod;
+                    ans = (ans*(crrPlnts + 1))%mod;
                     crrPlnts = 0;
                     s = 0;
                 }
             }
             if(corridor[i] == 'S') s++;
         }
-        if(s==1) return 0;
-        if(s==2 && ans == 0) return 1;
-        else return ans;
+        if(s==0 || s==1) return 0;
+        return ans;
     }
 };
