121ea6d482322a3b602848cbd03c44b0e3e1d58e
Awez Mirza <awezbaigmirza@gmail.com>
2023-12-08 14:47:37 +0530

    Time: 10 ms (78.50%) | Memory: 28.6 MB (34.93%) - LeetSync

--- 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
@@ -10,30 +10,11 @@ public:
             for(int j=0;j<n;j++){
                 mp[grid[i][j]][0] = i;
                 mp[grid[i][j]][1] = j;
-                cout<<grid[i][j]<<" ";
             }
-            cout<<endl;
         }
-
-
-        for(auto elem : mp){
-            cout<<elem[0]<<" ";
-            cout<<elem[1]<<" ";
-            cout<<"   ";
-        }
-
 
         for(int i = 1;i<n*n;i++){
-            if((abs(mp[i][0] - mp[i-1][0])) == 2 && (abs(mp[i][1] - mp[i-1][1])) == 1 ){
-                continue;
-            }
-            else if((abs( mp[i][0] - mp[i-1][0] )) == 1 && (abs(mp[i][1] - mp[i-1][1])) == 2 ){
-                continue;
-            }
-            else{
-                cout<<"false for i="<<i;
-                return false;
-            }
+            if(!(abs(mp[i][0] - mp[i-1][0]) == 2 && abs(mp[i][1] - mp[i-1][1]) == 1  || abs( mp[i][0] - mp[i-1][0] ) == 1 && abs(mp[i][1] - mp[i-1][1]) == 2 )) return false;
         }
         return true;
     }
