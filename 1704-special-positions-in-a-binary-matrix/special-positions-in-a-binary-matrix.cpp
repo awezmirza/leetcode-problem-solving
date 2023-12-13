725df398e67995acfa436903905c3c94171b0726
Awez Mirza <awezbaigmirza@gmail.com>
2023-12-13 17:38:33 +0530

    Time: 16 ms (68.95%) | Memory: 13.5 MB (12.89%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,47 +1,37 @@
 class Solution {
 
-// variable 
-// input
-// Extras fnctn call
-
-
 public:
-    bool isAnswer(int i,int j, vector<vector<int>>& mat){
-        int n = mat.size();
-        int m = mat[0].size();
-        for(int l = 0;l<m;l++){
-                        if(mat[i][l] == 1 && l != j){
-                            return false;
-                        }
-                    }
-        for(int l = 0;l<n;l++){
-            if(mat[l][j] == 1 && l != i){
-                return false;
-            }
-        }
-        return true;
-    }
 
     int numSpecial(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
         int ans = 0;
-        // vector<int> rows;
-        unordered_set <int> rows;
+        vector<int> rows(n);
+        vector<int> cols(m);
+        unordered_set <int> rowsTrack;
         for(int i = 0;i<n;i++){
-            if(rows.find(i) != rows.end()){
-                continue;
-            }
             for(int j=0;j<m;j++){
                 if(mat[i][j] == 1){
-                    if(isAnswer(i,j,mat)){
-                        rows.insert(i);
-                        ans++;
+                    rows[i]++;
+                    cols[j]++;
                     }
+            }
+        }
+
+        for(int i = 0;i<n;i++){
+             if(rowsTrack.find(i) != rowsTrack.end()){
+                continue;
+            }
+
+            for(int j=0;j<m;j++){
+                if(mat[i][j] == 1 && rows[i] ==1 && cols[j]==1){
+                    ans++;
+                    rowsTrack.insert(i);
                     break;
                 }
             }
         }
+
         return ans;
     }
 };
