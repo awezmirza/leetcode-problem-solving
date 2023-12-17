67bb59e6e63ca5bc66409a204aac0423d95c23b2
Awez Mirza <awezbaigmirza@gmail.com>
2023-12-17 22:50:34 +0530

    Time: 3 ms (41.97%) | Memory: 6.5 MB (33.80%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double solve(double x, long n) {
+        if(n == 0){
+            return 1;
+        }
+
+        if(n<0){
+            return solve(1/x,-n);
+        }
+
+        if( !(n&1) ){
+            return solve(x*x,n>>1);
+        }
+        return x*solve(x*x,n>>1);
+
+
+    }
+
+    double myPow(double x, int n) {
+        return solve(x, long(n));
+    }
+};
