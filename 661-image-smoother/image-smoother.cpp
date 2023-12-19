6ecd01ad37bbaadd12d8ead7726f298ff48841d4
Awez Mirza <awezbaigmirza@gmail.com>
2023-12-19 11:17:00 +0530

    Time: 50 ms (23.08%) | Memory: 21.9 MB (92.62%) - LeetSync

--- 661-image-smoother/image-smoother.cpp
+++ 661-image-smoother/image-smoother.cpp
@@ -1,9 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
-        int m = img.size(), n = img[0].size();
-        vector<vector<int>> ans(m, vector<int> (n));
+        // int m = img.size(), n = img[0].size();
+        // vector<vector<int>> ans(m, vector<int> (n));
 
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         int total = 0, cnt = 0;
+        //         for(int row = i-1;row<=i+1;row++){
+        //             for(int col = j-1;col<=j+1;col++){
+        //                 if(col < 0 || row < 0 || row >= m || col >= n){
+        //                     continue;
+        //                 }
+        //                 total += img[row][col];
+        //                 cnt++;
+        //             }
+        //         }
+        //         ans[i][j] = total/cnt;
+
+        //     }
+        // }
+        // return ans;
+
+        // In-place
+        int m = img.size(), n = img[0].size();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 int total = 0, cnt = 0;
@@ -12,14 +32,20 @@ public:
                         if(col < 0 || row < 0 || row >= m || col >= n){
                             continue;
                         }
-                        total += img[row][col];
+                        total += (img[row][col] % 256);
                         cnt++;
                     }
                 }
-                ans[i][j] = total/cnt;
-
+                img[i][j] |= (total/cnt << 8);
             }
         }
-        return ans;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                img[i][j] >>= 8;
+            }
+        }
+
+        return img;
     }
 };
