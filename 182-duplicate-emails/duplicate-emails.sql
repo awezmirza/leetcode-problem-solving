24380bedb4918b4064fb1079e3e94c2491095db2
Awez Mirza <awezbaigmirza@gmail.com>
2023-12-22 16:12:25 +0530

    Time: 665 ms (64.03%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-SELECT email AS Email FROM Person GROUP BY email HAVING COUNT(email) > 1;
+SELECT email FROM Person 
+GROUP BY email 
+HAVING COUNT(email) > 1;
