bc85bdf87242b4d5d9622589e5d9d45ea291dd45
Awez Mirza <awezbaigmirza@gmail.com>
2023-12-29 23:20:53 +0530

    Time: 9 ms (25.06%) | Memory: 7.6 MB (79.81%) - LeetSync

---
+++ 857-positions-of-large-groups/positions-of-large-groups.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> largeGroupPositions(string s) {
+        int n = s.size();
+        vector<vector<int>> ans;
+        for(int i = 0;i<n;i){
+            int last = i;
+            ++i;
+            while(s[i] == s[last] && i<n){ 
+                ++i;
+            }
+            if(i - last >= 3){
+                ans.push_back({last, i-1});
+            }
+        }
+        return ans;
+    }
+};
