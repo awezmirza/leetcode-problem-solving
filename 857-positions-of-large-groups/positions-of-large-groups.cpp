c9142ccf8167d0ac382d8ba504323c449b1a5829
Awez Mirza <awezbaigmirza@gmail.com>
2023-12-29 23:23:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (12.41%) - LeetSync

--- 857-positions-of-large-groups/positions-of-large-groups.cpp
+++ 857-positions-of-large-groups/positions-of-large-groups.cpp
@@ -4,14 +4,11 @@ public:
         int n = s.size();
         vector<vector<int>> ans;
         for(int i = 0;i<n;i){
-            int last = i;
-            ++i;
+            int last = i++;
             while(s[i] == s[last] && i<n){ 
                 ++i;
             }
-            if(i - last >= 3){
-                ans.push_back({last, i-1});
-            }
+            if(i - last >= 3) ans.push_back({last, i-1});
         }
         return ans;
     }
