6fc4216f8287eb9041f7cb249a51786ce7fcdcbd
Awez Mirza <awezbaigmirza@gmail.com>
2023-12-31 10:50:41 +0530

    Time: 5 ms (5.74%) | Memory: 6.6 MB (75.31%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -4,15 +4,15 @@ public:
         int n = s.size();
         int ans = -1;
         
-        vector<vector<int>> mp (26, vector<int> (2,0));
+        vector<pair <int,int>> mp (26, {0,0});
 
         for(int i = 0;i<n;i++){
-            if( mp[s[i] - 'a'][0] == 0 ){
-                mp[s[i] - 'a'][0] = 1;
-                mp[s[i] - 'a'][1] = i;
+            if( mp[s[i] - 'a'].first == 0 ){
+                mp[s[i] - 'a'].first = 1;
+                mp[s[i] - 'a'].second = i;
             } 
             else{
-                ans = max( ans, i - mp[s[i] - 'a'][1] -1 );
+                ans = max( ans, i - mp[s[i] - 'a'].second -1 );
             }
         }
 
