6122a995658795e5211a0a370584a5a9d4511941
Awez Mirza <awezbaigmirza@gmail.com>
2023-12-31 10:39:18 +0530

    Time: 3 ms (47.38%) | Memory: 6.8 MB (22.19%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -3,13 +3,19 @@ public:
     int maxLengthBetweenEqualCharacters(string s) {
         int n = s.size();
         int ans = -1;
+        
+        vector<vector<int>> mp (26, vector<int> (2,0));
+
         for(int i = 0;i<n;i++){
-            for(int j = i+1;j<n;j++){
-                if(s[i] == s[j]){
-                    ans = max(ans, j-i-1);
-                }
+            if( mp[s[i] - 'a'][0] == 0 ){
+                mp[s[i] - 'a'][0] = 1;
+                mp[s[i] - 'a'][1] = i;
+            } 
+            else{
+                ans = max( ans, i - mp[s[i] - 'a'][1] -1 );
             }
         }
+
         return ans;
     }
 };
