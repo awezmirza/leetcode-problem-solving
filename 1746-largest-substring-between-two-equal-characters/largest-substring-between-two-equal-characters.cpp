390f4e157dc01ccf1c3361e29746d01d7c3611f2
Awez Mirza <awezbaigmirza@gmail.com>
2023-12-31 11:06:46 +0530

    Time: 3 ms (47.38%) | Memory: 6.7 MB (41.15%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -4,15 +4,14 @@ public:
         int n = s.size();
         int ans = -1;
         
-        vector<pair <int,int>> mp (26, {0,0});
+        unordered_map<int,int> mp;
 
         for(int i = 0;i<n;i++){
-            if( mp[s[i] - 'a'].first == 0 ){
-                mp[s[i] - 'a'].first = 1;
-                mp[s[i] - 'a'].second = i;
+            if( mp.find(s[i] - 'a') == mp.end() ){
+                mp[s[i] - 'a'] = i;
             } 
             else{
-                ans = max( ans, i - mp[s[i] - 'a'].second -1 );
+                ans = max( ans, i - mp[s[i] - 'a'] -1 );
             }
         }
 
