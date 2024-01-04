2792de90e6e49265cc513bc59d968a86db1d7781
Awez Mirza <awezbaigmirza@gmail.com>
2024-01-04 15:34:37 +0530

    Time: 94 ms (98.71%) | Memory: 85.1 MB (63.17%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
+
+        unordered_map <int,int> mp;
+
+        for(int num: nums){
+            mp[num]++;
+        }
+
         int ans = 0;
-        int i = 0;
-        while(i<n){
-            int curr = nums[i];
-            int cnt = 1;
-            i++;
-            while(i<n && nums[i] == curr){
-                i++;
-                cnt++;
-            }
-            if(cnt == 1){
-                return -1;
-            }
+
+        for(auto [num, cnt] : mp){
+            if(cnt == 1) return -1; 
             if(cnt % 3 != 1 ){
                 ans += cnt/3;
                 cnt = cnt % 3;
@@ -27,11 +23,38 @@ public:
                 cnt += 3;
             }
             ans += cnt/2;
-            cnt = cnt % 2;
-            if(cnt>0){
-                return -1;
-            }
         }
         return ans;
+
+
+    //     int n = nums.size();
+    //     int ans = 0;
+    //     int i = 0;
+    //     while(i<n){
+    //         int curr = nums[i];
+    //         int cnt = 1;
+    //         i++;
+    //         while(i<n && nums[i] == curr){
+    //             i++;
+    //             cnt++;
+    //         }
+    //         if(cnt == 1) return -1; 
+    //         if(cnt % 3 != 1 ){
+    //             ans += cnt/3;
+    //             cnt = cnt % 3;
+    //         }
+    //         else{
+    //             ans += cnt/3;
+    //             ans--;
+    //             cnt = cnt % 3;
+    //             cnt += 3;
+    //         }
+    //         ans += cnt/2;
+    //         cnt = cnt % 2;
+    //         if(cnt>0){
+    //             return -1;
+    //         }
+    //     }
+    //     return ans;
     }
 };
