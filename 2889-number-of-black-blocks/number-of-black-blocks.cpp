1fc5b60c107756e24e4722ce20e869810b7033f9
Awez Mirza <awezbaigmirza@gmail.com>
2024-01-12 00:38:53 +0530

    Time: 834 ms (49.39%) | Memory: 265.1 MB (37.19%) - LeetSync

--- 2889-number-of-black-blocks/number-of-black-blocks.cpp
+++ 2889-number-of-black-blocks/number-of-black-blocks.cpp
@@ -1,21 +1,36 @@
-class Solution 
-{
+class Solution {
 public:
-    vector<long long> countBlackBlocks(int m, int n, vector<vector<int>>& coordinates) 
-    {
-        map<pair<int,int>,int> mp;
-        for(auto coordinate: coordinates)
-        {
-            int r=coordinate[0], c=coordinate[1];
-            if(r-1>=0 && c-1>=0) mp[{r-1,c-1}]++;
-            if(r-1>=0 && c+1<n) mp[{r-1,c}]++;
-            if(r+1<m && c-1>=0) mp[{r,c-1}]++;
-            if(r+1<m && c+1<n) mp[{r,c}]++;
+    vector<long long> countBlackBlocks(int m, int n,
+                                       vector<vector<int>>& coordinates) {
+        map<pair<int, int>, int> matrix;
+
+        for (auto pairs : coordinates) {
+
+            int r = pairs[0], c = pairs[1];
+
+            if (r - 1 >= 0 && c - 1 >= 0)
+                matrix[{r - 1, c - 1}]++;
+
+            if (r - 1 >= 0 && c < n - 1)
+                matrix[{r - 1,c}]++;
+
+            if (r < m - 1 && c - 1 >= 0)
+                matrix[{r, c - 1}]++;
+
+            if (r < m - 1 && c < n - 1)
+                matrix[{r, c}]++;
+
         }
-        vector<long long> ans(5,0);
-        for(auto it: mp) if(it.second<5) ans[it.second]++;
-        long long ans0=(long long)(m-1)*(n-1)-(long long)accumulate(ans.begin(),ans.end(),(long long)0);
-        ans[0]=ans0;
+
+        vector<long long> ans(5, 0);
+
+        for (auto mp : matrix) {
+            ans[mp.second]++;
+        }
+
+        long long allSum = ans[1] + ans[2] + ans[3] + ans[4];
+        ans[0] = (long long) (m - 1) * (n - 1) - allSum;
+
         return ans;
     }
 };
