8a4ecc3f4edaf223cda6ed35cdb570059b3f1354
Awez Mirza <awezbaigmirza@gmail.com>
2024-01-15 13:39:04 +0530

    Time: 586 ms (37.14%) | Memory: 184.1 MB (28.24%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        unordered_set<int> noLoss; // 1,2,5
-        unordered_set<int> oneLoss; // 6 
-        unordered_set<int> moreLoss; // 3
+        unordered_set<int> noLoss, oneLoss, moreLoss;
         for (auto match : matches) {
-            if (moreLoss.find(match[0]) == moreLoss.end()) {
-                if (oneLoss.find(match[0]) == oneLoss.end()) {
-                    if (noLoss.find(match[0]) == noLoss.end()) {
-                        noLoss.insert(match[0]);
-                    }
-                }
+            if (moreLoss.find(match[0]) == moreLoss.end() &&
+                oneLoss.find(match[0]) == oneLoss.end() &&
+                noLoss.find(match[0]) == noLoss.end()) {
+                noLoss.insert(match[0]);
             }
 
-            if (moreLoss.find(match[1]) == moreLoss.end()) {
-                if (oneLoss.find(match[1]) == oneLoss.end()) {
-                    oneLoss.insert(match[1]);
-                        noLoss.erase(match[1]);
-                } else {
-                    moreLoss.insert(match[1]);
-                    oneLoss.erase(match[1]);
-                }
+            if (moreLoss.find(match[1]) == moreLoss.end() &&
+                oneLoss.find(match[1]) == oneLoss.end()) {
+                oneLoss.insert(match[1]);
+                noLoss.erase(match[1]);
+            } else {
+                moreLoss.insert(match[1]);
+                oneLoss.erase(match[1]);
             }
         }
 
