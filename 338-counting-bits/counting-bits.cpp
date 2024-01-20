b4e3e08dea120737556e6fba40fd34c34ea038d5
Awez Mirza <awezbaigmirza@gmail.com>
2024-01-20 22:34:33 +0530

    Time: 9 ms (18.65%) | Memory: 9 MB (8.54%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
+
+    int solve(int n){
+        int cnt = 0;
+        while(n){
+            if(n&1) cnt++;
+            n >>= 1;
+        }
+        return cnt;
+    }
+
     vector<int> countBits(int n) {
         vector<int> ans(n+1,0);
         for(int i = 0;i<=n;i++){
-            ans[i] = (__builtin_popcount(i));
+            ans[i] = solve(i);
         }
         return ans;
     }
