6c9e602cd2e02e29228ac1d84bf7dc5624600f36
Awez Mirza <awezbaigmirza@gmail.com>
2024-01-20 22:34:44 +0530

    Time: 4 ms (56.98%) | Memory: 8.8 MB (27.55%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -12,9 +12,7 @@ public:
 
     vector<int> countBits(int n) {
         vector<int> ans(n+1,0);
-        for(int i = 0;i<=n;i++){
-            ans[i] = solve(i);
-        }
+        for(int i = 0;i<=n;i++) ans[i] = solve(i);
         return ans;
     }
 };
