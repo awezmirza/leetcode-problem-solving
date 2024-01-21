5c2d1dcf29297b7f1d3bd885a7aff0a4a05d20fe
Awez Mirza <awezbaigmirza@gmail.com>
2024-01-21 20:26:26 +0530

    Time: 158 ms (95.91%) | Memory: 295.6 MB (5.26%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
         vector<int> mp(200002);
+        int greatestElem = 0;
         for(int num:nums){
             if (num - k <= 0) {
                 mp[0]++;
@@ -10,11 +11,12 @@ public:
                 mp[num - k]++;
             }
             mp[num + k + 1]--;
+            greatestElem = max(greatestElem,num + k + 1);
         }
         int maxi = 0;
         int currSum=0;
-        for(int el :mp){
-            currSum += el;
+        for(int i=0;i<greatestElem;i++){
+            currSum += mp[i];
             maxi = max(maxi,currSum);
         }
         return maxi;
