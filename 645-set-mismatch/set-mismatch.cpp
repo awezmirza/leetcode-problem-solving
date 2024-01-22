7aff6fa75470e50deac1584037737ce71a93539a
Awez Mirza <awezbaigmirza@gmail.com>
2024-01-22 14:57:47 +0530

    Time: 31 ms (50.37%) | Memory: 24.8 MB (34.75%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -2,20 +2,13 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        // if (nums[0] != 1)
-        //     return {2, 1};
         int prev = 0;
         vector<int> ans;
         int num = 1;
-        for (int i = 0; i < n; i++) {
-            if(nums[i] == num){
-                num++;
-            }
-            if (nums[i] == prev) {
-                ans.push_back(prev);
-            }
-            prev = nums[i];
+        for (int el:nums) {
+            if(el == num) num++; 
+            if (el == prev) ans.push_back(prev); 
+            prev = el;
         }
         ans.push_back(num);
         return ans;
