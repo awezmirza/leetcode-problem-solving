fead63f9cee7369483bd4d7bd510ee5801487d9d
Awez Mirza <awezbaigmirza@gmail.com>
2024-01-24 16:02:56 +0530

    Time: 54 ms (98.19%) | Memory: 28 MB (86.75%) - LeetSync

--- 2877-shortest-string-that-contains-three-strings/shortest-string-that-contains-three-strings.cpp
+++ 2877-shortest-string-that-contains-three-strings/shortest-string-that-contains-three-strings.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     string fit(string fst, string scnd) {
-        int ptr1 = 0;
-        int ptr2 = 0;
-        int n = fst.size();
-        int m = scnd.size();
+        int ptr1 = 0, ptr2 = 0;
+        int n = fst.size(), m = scnd.size();
         string ans = "";
         for (int i = 0; i < n; i++) {
             int lastFound = -1;
-            ptr1 = i;
-            ptr2 = 0;
+            ptr1 = i, ptr2 = 0;
             if (fst[ptr1] == scnd[ptr2]) {
                 lastFound = ptr1;
                 while (fst[ptr1] == scnd[ptr2]) {
-                    if (ptr1 == n - 1) {
-                        return fst.substr(0, lastFound) + scnd;
-                    }
-                    if (ptr2 == m - 1) {
-                        return fst;
-                    }
+                    if (ptr1 == n - 1) return fst.substr(0, lastFound) + scnd;
+                    if (ptr2 == m - 1) return fst;
                     ptr1++, ptr2++;
                 }
             }
@@ -27,15 +20,9 @@ public:
     }
 
     string minimum(string a,string b){
-        if(a.size() == b.size()){
-            return min(a,b);
-        }
-        else if(a.size() < b.size()){
-            return a;
-        }
-        else{
-            return b;
-        }
+        if(a.size() == b.size()) return min(a,b); 
+        else if(a.size() < b.size()) return a; 
+        else return b;
     }
 
     string minimumString(string a, string b, string c) {
@@ -54,56 +41,8 @@ public:
         string sxth = fit(fit(c, b), a);
 
         string mini = minimum(fst,minimum(scnd,minimum(thrd,minimum(frth,minimum(ffth,sxth)))));
-        // string mini = minimum(fst,scnd);
-        // string mini = minimum(fst,scnd);
-        // string mini = minimum(fst,scnd);
-        // string mini = minimum(fst,scnd);
 
         return mini;
 
-        // int one = fst.length();
-        // int two = scnd.length();
-        // int three = thrd.length();
-        // int four = frth.length();
-        // int five = ffth.length();
-        // int six = sxth.length();
-
-        // string ans = "";
-
-        // int mini = min(one, min(two, min(three, min(four, min(five, six)))));
-        // if (mini == one) {
-        //     ans = fst;
-        // }
-        // if (mini == two) {
-        //     if (ans == "")
-        //         ans = scnd;
-        //     else
-        //         ans = min(ans, scnd);
-        // }
-        // if (mini == three) {
-        //     if (ans == "")
-        //         ans = thrd;
-        //     else
-        //         ans = min(ans, thrd);
-        // }
-        // if (mini == four) {
-        //     if (ans == "")
-        //         ans = frth;
-        //     else
-        //         ans = min(ans, frth);
-        // }
-        // if (mini == five) {
-        //     if (ans == "")
-        //         ans = ffth;
-        //     else
-        //         ans = min(ans, ffth);
-        // }
-        // if (mini == six) {
-        //     if (ans == "")
-        //         ans = sxth;
-        //     else
-        //         ans = min(ans, sxth);
-        // }
-        // return ans;
     }
 };
