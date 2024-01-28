c63e23907e188f8e63c5d07f3cbd81f1529ec26f
Awez Mirza <awezbaigmirza@gmail.com>
2024-01-28 19:58:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (8.84%) - LeetSync

--- 2955-account-balance-after-rounded-purchase/account-balance-after-rounded-purchase.cpp
+++ 2955-account-balance-after-rounded-purchase/account-balance-after-rounded-purchase.cpp
@@ -2,14 +2,8 @@ class Solution {
 public:
     int accountBalanceAfterPurchase(int purchaseAmount) {
         int n = purchaseAmount%10;
+        n = n>=5? 10: 0;
         int m = purchaseAmount/10;
-        cout<<"m is: "<<m<<"\n";
-        cout<<"n is: "<<n<<"\n";
-        if(n>=5){
-            n=10;
-        }
-        else n = 0;
-        m = m*10 + n;
-        return 100 - m;
+        return 100 - ((m*10) + n);
     }
 };
