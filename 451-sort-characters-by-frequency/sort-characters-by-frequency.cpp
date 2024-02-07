6cc805c2b053466bdfe6331bc101d8868da33bd7
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-07 19:58:26 +0530

    Time: 18 ms (15.70%) | Memory: 10.1 MB (35.10%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,19 +1,36 @@
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         vector<pair<int, char>> freq(256, {0, 0});
+//         for (auto ch : s) {
+//             freq[ch].first++;
+//             freq[ch].second = ch;
+//         }
+//         sort(freq.begin(), freq.end(),greater());
+//         string ans = "";
+//         for (auto el : freq) {
+//             while (el.first > 0) {
+//                 ans.push_back(el.second);
+//                 el.first--;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     string frequencySort(string s) {
-        vector<pair<int, char>> freq(256, {0, 0});
+        vector <int> freq(256, 0);
         for (auto ch : s) {
-            freq[ch].first++;
-            freq[ch].second = ch;
+            freq[ch]++;
         }
-        sort(freq.begin(), freq.end(),greater());
-        string ans = "";
-        for (auto el : freq) {
-            while (el.first > 0) {
-                ans.push_back(el.second);
-                el.first--;
-            }
-        }
-        return ans;
+        auto cmt = [&](char a, char b) { 
+            if(freq[a] == freq[b]) return a>b;
+            return freq[a] > freq[b];             
+        };
+        sort(s.begin(), s.end(), cmt);
+       
+        return s;
     }
 };
