a26add867379af6fa04089d00e49c7708bc87627
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-07 20:06:07 +0530

    Time: 7 ms (91.10%) | Memory: 9.7 MB (37.02%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,36 +1,36 @@
-// class Solution {
-// public:
-//     string frequencySort(string s) {
-//         vector<pair<int, char>> freq(256, {0, 0});
-//         for (auto ch : s) {
-//             freq[ch].first++;
-//             freq[ch].second = ch;
-//         }
-//         sort(freq.begin(), freq.end(),greater());
-//         string ans = "";
-//         for (auto el : freq) {
-//             while (el.first > 0) {
-//                 ans.push_back(el.second);
-//                 el.first--;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     string frequencySort(string s) {
-        vector <int> freq(256, 0);
+        vector<pair<int, char>> freq(256, {0, 0});
         for (auto ch : s) {
-            freq[ch]++;
+            freq[ch].first++;
+            freq[ch].second = ch;
         }
-        auto cmt = [&](char a, char b) { 
-            if(freq[a] == freq[b]) return a>b;
-            return freq[a] > freq[b];             
-        };
-        sort(s.begin(), s.end(), cmt);
-       
-        return s;
+        sort(freq.begin(), freq.end(),greater());
+        string ans = "";
+        for (auto el : freq) {
+            while (el.first > 0) {
+                ans.push_back(el.second);
+                el.first--;
+            }
+        }
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         vector <int> freq(256, 0);
+//         for (auto ch : s) {
+//             freq[ch]++;
+//         }
+//         auto cmt = [&](char a, char b) { 
+//             if(freq[a] == freq[b]) return a>b;
+//             return freq[a] > freq[b];             
+//         };
+//         sort(s.begin(), s.end(), cmt);
+       
+//         return s;
+//     }
+// };
