c229c6175a82c0e492e048981a392265749361ab
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-16 13:01:25 +0530

    Time: 85 ms (98.38%) | Memory: 49.3 MB (97.45%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-        sort(arr.begin(),arr.end());
+        sort(arr.begin(), arr.end());
         int n = arr.size();
         vector<int> freqs;
-        for(int i=0;i<n;){
+        int i = 0;
+        while (i < n) {
             int cnt = 1;
             int last = arr[i++];
-            while(i<n && arr[i] == last){
+            while (i < n && arr[i] == last) {
                 cnt++;
                 i++;
             }
             freqs.push_back(cnt);
         }
-        sort(freqs.begin(),freqs.end(),greater());
-        while(k>0 && freqs.size() > 0){
+        sort(freqs.begin(), freqs.end(), greater());
+        while (k > 0 && freqs.size() > 0) {
             int iterator = freqs.size() - 1;
-            if(freqs[iterator] > k){
+            if (freqs[iterator] > k) {
                 freqs[iterator] = freqs[iterator] - k;
                 k = 0;
-            }
-            else{
+            } else {
                 k = k - freqs[iterator];
                 freqs.pop_back();
             }
