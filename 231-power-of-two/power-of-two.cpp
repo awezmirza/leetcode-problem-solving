646d4038297e185daf06ef0feca5845e74370a81
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-19 13:25:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (19.05%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,12 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<1) return false;
-        if(n==0 || n==1) return true;
-        while(n>=2){
-            if(n==2) return true;
-            if(n%2) return false;
-            n = n/2;
-        }
+        if(__builtin_popcount(n) == 1) return true;
         return false;
     }
 };
