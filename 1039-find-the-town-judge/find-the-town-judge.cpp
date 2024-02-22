934d96108a8b18a3c95ac8298d57a7b4b54110b1
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-22 14:55:45 +0530

    Time: 107 ms (96.80%) | Memory: 64.3 MB (69.38%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<bool> isJudge(n + 1,true);
-        vector<int> isKnownBy(n + 1,0);
+        vector<bool> isJudge(n,true);
+        vector<int> isKnownBy(n,0);
         for(vector<int>& tr:trust){
-            isJudge[tr[0]] = false;
-            isKnownBy[tr[1]]++;
+            isJudge[tr[0]-1] = false;
+            isKnownBy[tr[1]-1]++;
         }
-        for(int i = 1; i<=n;i++){
-            if(isJudge[i] && isKnownBy[i] == n-1) return i;
+        for(int i = 0; i<n;i++){
+            if(isJudge[i] && isKnownBy[i] == n-1) return i+1;
         }
         return -1;
     }
