febb498594a004623792878cd57c72e961fd90f9
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-23 20:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (15.11%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -13,9 +13,20 @@ public:
         return ans;
     }
 
-    int climbStairs(int n) {
+    int climbStairsHelperTab(int n){
+
         vector<int> dp(n+1,-1);
-        int ans = climbStairsHelper(n,dp);
+        dp[1] = 1;
+        if(n>1)
+        dp[2] = 2;
+        for(int i = 3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+    int climbStairs(int n) {
+        int ans = climbStairsHelperTab(n);
         return ans;
     }
 };
