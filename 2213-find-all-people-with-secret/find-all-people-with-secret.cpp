6f559fdc6017b0672ee00596ecd8e6d8fc2ca149
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-24 13:36:43 +0530

    Time: 581 ms (48.36%) | Memory: 231.3 MB (24.59%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
         unordered_map< int , set<pair<int,int> > > allMeet;
+        // O(nLgn)
         for(auto& meet: meetings){
              int p1 = meet[0];
              int p2 = meet[1];
@@ -17,18 +18,19 @@ public:
         q.push({0,firstPerson});
         q.push({0,0});
 
+        // O(n)
         while(!q.empty()){
             auto qFrnt = q.front();
             q.pop();
             int fTime = qFrnt.first;
             int fPerson = qFrnt.second;
 
-            for(auto [stTpTime, stTpPrsn]: allMeet[fPerson] ){
-                if(stTpTime >= fTime){
-                    ans.insert(stTpPrsn);
-                    q.push({stTpTime, stTpPrsn});
-                    allMeet[fPerson].erase({stTpTime, stTpPrsn});
-                }
+            while(!allMeet[fPerson].empty()){
+                auto [stTpTime, stTpPrsn] = *allMeet[fPerson].rbegin();
+                if(stTpTime < fTime) break;
+                ans.insert(stTpPrsn);
+                q.push({stTpTime, stTpPrsn});
+                allMeet[fPerson].erase({stTpTime, stTpPrsn});
             }
         }
         vector<int> vecAns(ans.begin(), ans.end());
