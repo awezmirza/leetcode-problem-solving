f4940330e8ece285b58cbec5fbbd56b48fc4a817
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-24 22:40:10 +0530

    Time: 2 ms (40.46%) | Memory: 9.3 MB (7.95%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    int robHelper(vector<int>& nums, int lastIdx, vector<int>& dp){
-        if(lastIdx >= nums.size()){
-            return 0;
+
+    int robHelperSO(vector<int>& nums){
+        int n = nums.size();
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        for(int i = 1;i < n; i++){
+            int leave = dp[i-1];
+
+            int take = nums[i];
+            if(i > 1) take+= dp[i-2];
+
+            dp[i] = max(take,leave);
         }
-        if(dp[lastIdx] != -1 ) return dp[lastIdx] ;
-        int take = nums[lastIdx] + robHelper(nums,lastIdx + 2, dp);
-        int leave = robHelper(nums , lastIdx + 1, dp);
-        dp[lastIdx] = max(take, leave);
-        return dp[lastIdx];
+        return dp[n-1];
     }
 
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        int ans = robHelper(nums,0, dp);
+        
+        int ans = robHelperSO(nums);
         return ans;
     }
 };
