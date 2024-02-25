2184c3e9c0bd930f1c7b1161f0d050e0bd14abf2
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-25 18:04:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (8.69%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -8,26 +8,41 @@ public:
     //     return max(take, leave);
     // }
 
-    int robHelper(vector<int>& nums, int i, vector<int>& dp){
-        if(i >= nums.size()) return 0;
-        if(dp[i] != -1) return dp[i]; 
-        int take = nums[i] + robHelper(nums, i+2, dp);
-        int leave = robHelper(nums, i+1, dp);
-        return dp[i] = max(take, leave);
+    // int robHelper(vector<int>& nums, int i, vector<int>& dp){
+    //     if(i >= nums.size()) return 0;
+    //     if(dp[i] != -1) return dp[i]; 
+    //     int take = nums[i] + robHelper(nums, i+2, dp);
+    //     int leave = robHelper(nums, i+1, dp);
+    //     return dp[i] = max(take, leave);
+    // }
+
+    int robHelper(vector<int>& nums){
+        
+        int n = nums.size();
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        for(int i = 1;i<n;i++){
+            int leave = dp[i-1];
+            int take = nums[i];
+            if(i>1) 
+                take += dp[i-2];
+            dp[i] = max(take, leave);
+        }
+        return dp[n-1];
+
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return nums[0];
+        if(n == 1) return nums[0];
         vector<int> arrayWithoutFirst, arrayWithoutLast;
         for(int i = 0;i<n;i++){
             if(i!=0) arrayWithoutFirst.push_back(nums[i]);
             if(i!=n-1) arrayWithoutLast.push_back(nums[i]);
         }
-        vector<int> dp1(n,-1);
-        vector<int> dp2(n,-1);
-        int ans1 = robHelper(arrayWithoutFirst,0, dp1);
-        int ans2 = robHelper(arrayWithoutLast,0, dp2);
+
+        int ans1 = robHelper(arrayWithoutFirst);
+        int ans2 = robHelper(arrayWithoutLast);
         return max(ans1,ans2);
     }
 };
