00f909f53ed109fd135ab6420f9f05478f8fc7d9
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-26 13:57:27 +0530

    Time: 3 ms (34.18%) | Memory: 7.4 MB (44.97%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
-
-    // int helper(vector<vector<int>>& grid, int x, int y, int destx, int desty){
-    //     if(x == destx && y == desty) return 1;
-    //     if(grid[x][y] != -1) return grid[x][y];
-    //     int right = 0;
-    //     if(x != destx) right = helper(grid, x + 1, y, destx, desty);
-    //     int down = 0;
-    //     if(y != desty) down = helper(grid, x, y + 1, destx, desty);
-    //     return grid[x][y] = right + down;
-    // }
-
-    int helperTab(int m, int n){
+    int uniquePaths(int m, int n) {
         vector<int> temp(n,1);
         for(int x = m - 2;x>=0;x--){
             vector<int> curr(n,1);
@@ -21,10 +10,5 @@ public:
             temp = curr;
         }
         return temp[0];
-    }
-
-    int uniquePaths(int m, int n) {
-        int ans = helperTab(m, n);
-        return ans;
     }
 };
