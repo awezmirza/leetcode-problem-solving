9e2e6389abaf45b3fb8082e770bdd740b9f3e430
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-26 11:56:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (5.64%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -15,8 +15,35 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(!p && !q) return true;
         if(!p && q || !q && p || p->val != q->val) return false;
-        bool forLeft = isSameTree(p->left,q->left);
-        bool forRight = isSameTree(p->right,q->right);
-        return forLeft && forRight;
+        queue<TreeNode*> pQue;
+        pQue.push(p);
+        queue<TreeNode*> qQue;
+        qQue.push(q);
+        while(!pQue.empty() || !qQue.empty()){
+            TreeNode* pFr = pQue.front();
+            TreeNode* qFr = qQue.front();
+            pQue.pop();
+            qQue.pop();
+            if(pFr->val != qFr-> val) return false;
+            if(pFr->left){
+                if(!qFr->left) return false;
+                pQue.push(pFr->left);
+            } 
+                
+            if(qFr->left){
+                if(!qFr->left) return false;
+                qQue.push(qFr->left);
+            }
+
+            if(pFr->right){
+                if(!qFr->right) return false;
+                pQue.push(pFr->right);
+            }
+            if(qFr->right){
+                if(!pFr->right) return false;
+                qQue.push(qFr->right);
+            }
+        }
+        return pQue.empty() && qQue.empty();
     }
 };
