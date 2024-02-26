7ddea42d960307deeeb8246e9f8ec168f4c9d706
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-26 21:16:24 +0530

    Time: 11 ms (5.79%) | Memory: 10.2 MB (41.00%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,22 +1,62 @@
 class Solution {
-    int helper(vector<vector<int>>& obstacleGrid, int x, int y, vector<vector<int>>& dp){
+    // int helper(vector<vector<int>>& obstacleGrid, int x, int y, vector<vector<int>>& dp){
+    //     int maxX = obstacleGrid.size();
+    //     int maxY = obstacleGrid[0].size();
+    //     if(x >= maxX || y>= maxY || obstacleGrid[x][y] == 1)
+    //         return 0;
+    //     if(x == maxX-1 && y == maxY - 1) 
+    //         return 1;
+    //     if(dp[x][y] != -1)
+    //         return dp[x][y];
+    //     int down = helper(obstacleGrid, x , y + 1, dp);
+    //     int left = helper(obstacleGrid, x + 1 , y, dp);
+    //     return dp[x][y] = down + left;
+    // }
+
+    int helper(vector<vector<int>>& obstacleGrid){
         int maxX = obstacleGrid.size();
         int maxY = obstacleGrid[0].size();
-        if(x >= maxX || y>= maxY || obstacleGrid[x][y] == 1)
-            return 0;
-        if(x == maxX-1 && y == maxY - 1) 
-            return 1;
-        if(dp[x][y] != -1)
-            return dp[x][y];
-        int down = helper(obstacleGrid, x , y + 1, dp);
-        int left = helper(obstacleGrid, x + 1 , y, dp);
-        return dp[x][y] = down + left;
+        vector<vector<int>> dp(maxX, vector<int> (maxY, 0));
+        for(int i = maxX-1;i>=0;i--){
+            if(obstacleGrid[i][maxY-1] == 1) break;
+            dp[i][maxY-1] = 1;
+        }
+        for(int i = maxY-1;i>=0;i--){
+            if(obstacleGrid[maxX-1][i] == 1) break;
+            dp[maxX-1][i] = 1;
+        }
+        for(int i = 0;i<maxX;i++){
+            for(int j = 0;j<maxY;j++){
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<"After"<<endl;
+
+        for(int i = maxX-2;i>=0;i--){
+            for(int j = maxY-2;j>=0;j--){
+                if(i+1 > maxX || j+1 > maxY || obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else{
+                    int down = dp[i][j + 1];
+                    int left = dp[i + 1][j];
+                    if((long long)left + down < INT_MAX)
+                    dp[i][j] = left + down;
+                }
+            }
+        }
+        for(int i = 0;i<maxX;i++){
+            for(int j = 0;j<maxY;j++){
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return dp[0][0];
     }
 
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        vector<vector<int>> dp(obstacleGrid.size(), vector<int> (obstacleGrid[0].size(), -1));
-        int ans = helper(obstacleGrid, 0 , 0, dp);
+        
+        int ans = helper(obstacleGrid);
         return ans;
     }
 };
