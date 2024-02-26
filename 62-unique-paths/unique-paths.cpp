cde834d9b76460ee1f173da22799a6940bd68c99
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-26 13:26:14 +0530

    Time: 3 ms (34.18%) | Memory: 7.4 MB (42.80%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -12,27 +12,11 @@ public:
     // }
 
     int helperTab(int destx, int desty){
-        vector<vector<int>> grid(destx , vector<int> (desty,0));
-        for(int i = 0;i<destx;i++){
-            grid[i][desty -1] = 1;
-        }
-        for(int j = 0;j<desty;j++){
-            grid[destx-1][j] = 1;
-        }
+        vector<vector<int>> grid(destx , vector<int> (desty,1));
         for(int x = destx - 2;x>=0;x--){
             for(int y = desty-2;y>=0;y--){
-                int right = 0;
-                if(x != destx - 1) right = grid[x + 1][y];
-                int down = 0;
-                if(y != desty - 1) down = grid[x][y + 1];
-                grid[x][y] = right + down;
+                grid[x][y] = grid[x + 1][y] + grid[x][y + 1];
             }
-        }
-        for(auto el:grid){
-            for(auto e:el){
-                cout<<e<<" ";
-            }
-            cout<<"\n";
         }
         return grid[0][0];
     }
