a448f03b5fc8866201d9ea157d364568949ebc5e
Awez Mirza <awezbaigmirza@gmail.com>
2024-02-26 11:42:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (25.40%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -12,16 +12,11 @@
 class Solution {
 public:
 
-    bool doDFSAndCheck(TreeNode* p, TreeNode* q){
+    bool isSameTree(TreeNode* p, TreeNode* q) {
         if(!p && !q) return true;
         if(!p && q || !q && p || p->val != q->val) return false;
-        bool forLeft = doDFSAndCheck(p->left,q->left);
-        bool forRight = doDFSAndCheck(p->right,q->right);
+        bool forLeft = isSameTree(p->left,q->left);
+        bool forRight = isSameTree(p->right,q->right);
         return forLeft && forRight;
-    }
-
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        bool ans = doDFSAndCheck(p,q);
-        return ans;
     }
 };
