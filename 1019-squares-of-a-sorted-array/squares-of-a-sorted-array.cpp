815100cba5ba72bcbf883e2efb2d9c21bfc2b146
Awez Mirza <awezbaigmirza@gmail.com>
2024-03-02 19:44:58 +0530

    Time: 21 ms (73.33%) | Memory: 28.5 MB (46.74%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -4,17 +4,15 @@ public:
         int n = nums.size();
         vector<int> ans(n);
         int left = 0, right = n-1;
-        int ptr = n-1;
-        while(left<=right){
+        for(int i = n-1;i>=0;--i){
             if(abs(nums[left]) <= abs(nums[right])){
-                ans[ptr] = nums[right]*nums[right];
+                ans[i] = nums[right]*nums[right];
                 right--;
             }
             else{
-                ans[ptr] = nums[left]*nums[left];
+                ans[i] = nums[left]*nums[left];
                 left++;
             }
-            ptr--;
         }
         return ans;
     }
