ea41ffccad28fbc65bcf0f8bdab2f0ee92a1f548
Awez Mirza <awezbaigmirza@gmail.com>
2024-03-04 22:50:13 +0530

    Time: 5 ms (73.65%) | Memory: 12.4 MB (54.25%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -12,11 +12,31 @@ public:
         return dp[i][j] = min(left,down);
     }
 
-    int minPathSum(vector<vector<int>>& grid) {
+    int solveSO(vector<vector<int>>& grid){
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>dp (m, vector<int> (n,-1));
-        int ans = solve(grid,0, 0, dp);
+        vector<vector<int>>dp (m, vector<int> (n,0));
+        dp[m-1][n-1] = grid[m-1][n-1];
+        for(int i = m-2;i>=0;i--){
+            dp[i][n-1] = grid[i][n-1] + dp[i+1][n-1];
+        }
+        for(int j = n-2;j>=0;j--){
+            dp[m-1][j] = grid[m-1][j] + dp[m-1][j+1];
+        }
+
+        for(int i = m-2;i>=0;i--){
+            for(int j = n-2;j>=0;j--){
+                int left = grid[i][j] + dp[i][j+1];
+                int down = grid[i][j] + dp[i+1][j];
+                dp[i][j] = min(left, down);
+            }
+        }
+        return dp[0][0];
+    }
+
+    int minPathSum(vector<vector<int>>& grid) {
+
+        int ans = solveSO(grid);
         return ans;
     }
 };
