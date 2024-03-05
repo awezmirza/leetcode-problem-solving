84990aca6342109f48e33abcf65b1c15b0f69653
Awez Mirza <awezbaigmirza@gmail.com>
2024-03-05 20:36:19 +0530

    Time: 4 ms (74.94%) | Memory: 11.5 MB (21.58%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -9,17 +9,28 @@ public:
             return 1000000;
         }
         if(dp[row][col] != -1) return dp[row][col];
-
         int left = triangle[row][col] + solve(triangle, row + 1, col, dp);
         int right = triangle[row][col] + solve(triangle, row + 1, col + 1, dp);
         return dp[row][col] = min(left, right);
     }
 
-    int minimumTotal(vector<vector<int>>& triangle) {
+    int solveSO(vector<vector<int>>& triangle){
         int m = triangle.size();
         int mxColSze = triangle[m-1].size();
-        vector<vector<int>> dp(m,vector<int> (mxColSze, -1));
-        int ans = solve(triangle, 0, 0, dp);
+        vector<vector<int>> dp(m +1,vector<int> (m+1, 0));
+
+        for(int row = m-1;row>=0;--row){
+            for(int col = triangle[row].size()-1; col >=0;col-- ){
+                int left = triangle[row][col] + dp[row + 1][col];
+                int right = triangle[row][col] + dp[row + 1][col + 1];
+                dp[row][col] = min(left, right);
+            }
+        }
+        return dp[0][0];
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int ans = solveSO(triangle);
         return ans;
     }
 };
