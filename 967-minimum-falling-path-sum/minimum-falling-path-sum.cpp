ea5dad750b7bc022f0b525935412ea1b7f98ef25
Awez Mirza <awezbaigmirza@gmail.com>
2024-03-07 02:49:29 +0530

    Time: 17 ms (10.75%) | Memory: 14.2 MB (5.28%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -46,18 +46,22 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
         vector<vector<int>> dp(rows + 1, vector<int> (cols, 0));
+
+        vector<int> lastRow(cols, 0);
         for(int j = rows - 1;j >= 0;--j){
+            vector<int> currRow(cols, 0);
             for(int i = 0;i<cols;i++){
-                int miniR = miniR = matrix[j][i] + dp[j + 1][i];
-                if(i != cols - 1) miniR = min(miniR, matrix[j][i] + dp[j + 1][i + 1]);
-                if(i != 0) miniR = min(miniR, matrix[j][i] + dp[j + 1][i - 1]);
-                dp[j][i] = miniR;
+                int miniR = miniR = matrix[j][i] + lastRow[i];
+                if(i != cols - 1) miniR = min(miniR, matrix[j][i] + lastRow[i + 1]);
+                if(i != 0) miniR = min(miniR, matrix[j][i] + lastRow[i - 1]);
+                currRow[i] = miniR;
             }
+            lastRow = currRow;
         }
-        
+
         int mini = INT_MAX;
         for(int i = 0;i<cols;i++){
-            mini = min(dp[0][i], mini);
+            mini = min(lastRow[i], mini);
         }
         return mini;
     }
