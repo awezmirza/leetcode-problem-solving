38c9b14b376e2f9925735beb4b7713ee372b96f0
Awez Mirza <awezbaigmirza@gmail.com>
2024-03-07 01:46:42 +0530

    Time: 11 ms (67.02%) | Memory: 13.6 MB (6.83%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    int solveHelper(vector<vector<int>>& matrix, int row, int col, vector<vector<int>>& dp){
+
+        if(row >= matrix.size()) return 0;
+        if(col<0 || col >= matrix[0].size()) return 1000000;
+        if(dp[row][col] != -1000000){
+            return dp[row][col];
+        }
+
+        int mini = INT_MAX;
+        mini = min(mini, matrix[row][col] + solveHelper(matrix, row + 1, col, dp));
+        mini = min(mini, matrix[row][col] + solveHelper(matrix, row + 1, col + 1, dp));
+        mini = min(mini, matrix[row][col] + solveHelper(matrix, row + 1, col - 1, dp));
+        return dp[row][col] = mini;
+    }
+
+    int solve(vector<vector<int>>& matrix){
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int mini = INT_MAX;
+        vector<vector<int>> dp(rows, vector<int> (cols, -1000000));
+        for(int i = 0;i<cols;i++){
+            mini = min(mini, solveHelper(matrix, 0, i, dp));
+            mini = min(mini, solveHelper(matrix, 0, i + 1, dp));
+            mini = min(mini, solveHelper(matrix, 0, i - 1, dp));
+        }
+        return mini;
+    }
+
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int ans = solve(matrix);
+        return ans;
+    }
+};
