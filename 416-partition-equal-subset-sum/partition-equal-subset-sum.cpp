9b4f6e4be05f934cbd7084f15316ecd6c4a57082
Awez Mirza <awezbaigmirza@gmail.com>
2024-03-09 17:49:48 +0530

    Time: 149 ms (60.39%) | Memory: 97.5 MB (38.19%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -4,7 +4,7 @@ public:
     bool solve(vector<int>& nums, int target, int index, vector<vector<int>>& dp){
         if(target == 0) return true;
         if(index >= nums.size() || target < 0) return false;
-        
+
         if(dp[index][target] != -1) return dp[index][target];
 
         bool take = solve(nums, target - nums[index], index+1, dp);
@@ -13,13 +13,31 @@ public:
         return dp[index][target] = take | leave;
     }
 
+    bool solveTab(vector<int>& nums, int target){
+        vector<vector<int>> dp(nums.size(), vector<int> (target + 1, 0));
+        for(int i = 0;i<nums.size();i++){
+            dp[i][0] = 1;
+        }
+        for(int index = nums.size()-2;index>=0;--index){
+            for(int tgt = 0;tgt <= target;tgt++){
+                bool take = false;
+                if(tgt - nums[index] >= 0)
+                    take = dp[index+1][tgt - nums[index]];
+                bool leave = dp[index+1][tgt];
+                dp[index][tgt] = take | leave;
+            }
+        }
+        return dp[0][target];
+    }
+
+
     bool canPartition(vector<int>& nums) {
         int totalSum = 0;
         for(int num: nums) totalSum += num;
         if(totalSum%2 == 1) return false;
         int target = totalSum/2;
-        vector<vector<int>> dp(nums.size(), vector<int> (target + 1, -1));
-        bool ans = solve(nums,target, 0, dp);
+
+        bool ans = solveTab(nums,target);
         return ans;
     }
 };
