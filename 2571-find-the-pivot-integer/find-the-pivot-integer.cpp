8020367727fcae13c3b052ee4a65a4e949c463d8
Awez Mirza <awezbaigmirza@gmail.com>
2024-03-13 19:58:18 +0530

    Time: 5 ms (18.83%) | Memory: 8.7 MB (8.18%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,21 +1,34 @@
+// class Solution {
+// public:
+//     int pivotInteger(int n) {
+//         vector<int> pfSum(n+1);
+//         for(int i = 1;i<=n;i++){
+//             pfSum[i] = i + pfSum[i-1];
+//         }
+//         int totSum = pfSum[n];
+//         int st = 1, end = n;
+//         while(st<=end){
+//             int mid = (st + end)/2;
+//             if(pfSum[mid] == totSum - pfSum[mid - 1]) return mid;
+//             else if(pfSum[mid] < totSum - pfSum[mid - 1]){
+//                 st = mid + 1;
+//             }
+//             else{
+//                 end = mid -1;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int pivotInteger(int n) {
         vector<int> pfSum(n+1);
+        int totSum = (n*(n+1))/2;
         for(int i = 1;i<=n;i++){
             pfSum[i] = i + pfSum[i-1];
-        }
-        int totSum = pfSum[n];
-        int st = 1, end = n;
-        while(st<=end){
-            int mid = (st + end)/2;
-            if(pfSum[mid] == totSum - pfSum[mid - 1]) return mid;
-            else if(pfSum[mid] < totSum - pfSum[mid - 1]){
-                st = mid + 1;
-            }
-            else{
-                end = mid -1;
-            }
+            if(pfSum[i] == totSum - pfSum[i - 1]) return i;
         }
         return -1;
     }
