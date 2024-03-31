88ff2dd67659769effd92fa3cd11aa1ac0a105c8
Awez Mirza <awezbaigmirza@gmail.com>
2024-03-31 16:55:16 +0530

    Time: 85 ms (75.27%) | Memory: 82.5 MB (53.99%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
@@ -2,33 +2,20 @@ class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
         int n = nums.size();
-        int minFnd = -1;
-        int maxFnd = -1;
+        int minFnd = -1, maxFnd = -1;
         long long ans = 0;
         int j = 0;
         for(int i=0;i<n;i++){
-            if(nums[i] == minK ){
+            if(nums[i] == minK)
                 minFnd = i;
-            }
-            if(nums[i] == maxK){
+            if(nums[i] == maxK)
                 maxFnd = i;
-            }
             if(nums[i] > maxK || nums[i] < minK){
-                maxFnd = -1;
-                minFnd = -1;
-                j = i+1;
+                maxFnd = -1, minFnd = -1;
+                j = i + 1;
             }
-            if(minFnd!=-1 && maxFnd!=-1){
-                cout<<"in for i:"<<i<<endl;
-                if(minFnd > maxFnd){
-                    int toSub = i - maxFnd;
-                    ans += (i-j+1) - toSub;
-                }
-                else{
-                    int toSub = i - minFnd;
-                    ans += (i-j+1) - toSub;
-                }
-            }
+            if(minFnd!=-1 && maxFnd!=-1)
+                ans += (i-j+1) - (i - min(minFnd,maxFnd));
         }
         return ans;
     }
