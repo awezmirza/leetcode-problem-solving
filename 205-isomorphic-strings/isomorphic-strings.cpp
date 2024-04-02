56dc59231b299222dbd92e44b4b20a334374e428
Awez Mirza <awezbaigmirza@gmail.com>
2024-04-03 00:05:15 +0530

    Time: 7 ms (48.84%) | Memory: 9.4 MB (42.66%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,37 +1,19 @@
-// class Solution {
-// public:
-//     bool isIsomorphic(string s, string t) {
-//         unordered_map<char,char> mp;
-//         vector<bool> used(256);
-//         int n = s.size();
-//         for(int i = 0;i<n;i++){
-//             if(mp.count(s[i])){
-
-//                 return false;
-//             }
-//             else{
-//                 mp[t[i]] = s[i];
-//             }
-//         }
-//         return true;
-//     }
-// };
-
-
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int arrs[256] = {0};
-        bool arrt[256] = {0};
-        for(int i=0;i<s.size();i++){
-            if(arrs[s[i]] == 0 && arrt[t[i]] == 0 ){
-            arrs[s[i]] = t[i];
-            arrt[t[i]] = 1;
+        unordered_map<char,char> mp;
+        vector<bool> used(256);
+        int n = s.size();
+        for(int i = 0;i<n;i++){
+            if(mp.count(s[i])){
+                if(mp[s[i]] != t[i]){
+                    return false;
+                }
             }
-        }
-        for(int i=0;i<s.size();i++){
-            if(arrs[s[i]] != t[i]){
-            return false;
+            else{
+                if(used[t[i]]) return false;
+                mp[s[i]] = t[i];
+                used[t[i]] = true;
             }
         }
         return true;
