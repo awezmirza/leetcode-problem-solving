e724e24306e2af3268e3d91a9f84478b88795d61
Awez Mirza <awezbaigmirza@gmail.com>
2024-04-18 15:45:24 +0530

    Time: 44 ms (99.86%) | Memory: 100.6 MB (94.99%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,63 +1,32 @@
 class Solution {
 public:
-    int findPerimeter(vector<vector<int>>& grid, int i, int j){
-
-        int m = grid.size();
-        int n = grid[0].size();
-
-        int currPerim = 0;
-
-        grid[i][j] = -1;
-
-        if(i > 0 && grid[i - 1][j] == 1){
-            currPerim += findPerimeter(grid, i - 1, j);
-        }
-        else if(i == 0 || grid[i - 1][j] == 0){
-            currPerim++;
-        }
-
-
-        if(i < m - 1 && grid[i + 1][j] == 1){
-            currPerim += findPerimeter(grid, i + 1, j);
-        }
-        else if(i == m - 1 || grid[i + 1][j] == 0){
-            currPerim++;
-        }
-
-        if(j > 0 && grid[i][j - 1] == 1){
-            currPerim += findPerimeter(grid, i, j - 1);
-        }
-        else if(j == 0 || grid[i][j - 1] == 0){
-            currPerim++;
-        }
-
-        if(j < n - 1 && grid[i][j + 1] == 1){
-            currPerim += findPerimeter(grid, i, j + 1);
-        }
-        else if(j == n - 1 || grid[i][j + 1] == 0){
-            currPerim++;
-        }
-
-        return currPerim;
-    }
-
     int islandPerimeter(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        int i = 0;
-        int j = 0;
+        int ans = 0;
 
-        while(grid[i][j] != 1){
-            j++;
-            if(j >= n){
-                i++;
-                j = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    if (i == 0 || grid[i - 1][j] == 0 ) {
+                        ans++;
+                    }
+                    
+                    if (i == m - 1 || grid[i + 1][j] == 0 ) {
+                        ans++;
+                    }
+
+                    if (j == 0 || grid[i][j - 1] == 0 ) {
+                        ans++;
+                    }
+
+                    if (j == n - 1 || grid[i][j + 1] == 0 ) {
+                        ans++;
+                    }
+                }
             }
         }
-
-        int perimeter = findPerimeter(grid, i, j);
-
-        return perimeter;
+        return ans;
     }
 };
