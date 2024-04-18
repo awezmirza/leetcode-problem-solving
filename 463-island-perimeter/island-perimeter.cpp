d66a5ecbc009c613f54e9f60146031a29e8a947e
Awez Mirza <awezbaigmirza@gmail.com>
2024-04-18 15:38:55 +0530

    Time: 59 ms (94.46%) | Memory: 101.2 MB (27.95%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    int findPerimeter(vector<vector<int>>& grid, int i, int j){
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if(i < 0 || j < 0 || i >= m || j >= n){
+            return 0;
+        }
+
+        int currPerim = 0;
+
+        grid[i][j] = -1;
+
+        if(i > 0 && grid[i - 1][j] == 1){
+            currPerim += findPerimeter(grid, i - 1, j);
+        }
+        else if(i == 0 || grid[i - 1][j] == 0){
+            currPerim++;
+        }
+
+
+        if(i < m - 1 && grid[i + 1][j] == 1){
+            currPerim += findPerimeter(grid, i + 1, j);
+        }
+        else if(i == m - 1 || grid[i + 1][j] == 0){
+            currPerim++;
+        }
+
+        if(j > 0 && grid[i][j - 1] == 1){
+            currPerim += findPerimeter(grid, i, j - 1);
+        }
+        else if(j == 0 || grid[i][j - 1] == 0){
+            currPerim++;
+        }
+
+        if(j < n - 1 && grid[i][j + 1] == 1){
+            currPerim += findPerimeter(grid, i, j + 1);
+        }
+        else if(j == n - 1 || grid[i][j + 1] == 0){
+            currPerim++;
+        }
+
+        return currPerim;
+    }
+
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int i = 0;
+        int j = 0;
+
+        while(grid[i][j] != 1){
+            j++;
+            if(j >= n){
+                i++;
+                j = 0;
+            }
+        }
+
+        int perimeter = findPerimeter(grid, i, j);
+
+        return perimeter;
+    }
+};
