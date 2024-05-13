7dcbbafcdcc312e9c2c4a9029f442b01855ccea8
Awez Mirza <awezbaigmirza@gmail.com>
2024-05-13 23:17:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (94.44%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -3,35 +3,17 @@ public:
     int matrixScore(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        for(int i = 0; i < m; i++){
-            if(grid[i][0] == 0){
-                for(int j = 0; j < n; j++){
-                    grid[i][j] ^= 1;
-                }
-            }
-        }
+
+        int ans = m * (1 << (n - 1));
 
         for(int j = 1; j < n; j++){
             int colCnt = 0;
             for(int i = 0; i < m; i++){
-                if(grid[i][j] == 1){
-                    colCnt++;
-                }
+                int curr = grid[i][0] == 1 ? grid[i][j] : grid[i][j] ^ 1;
+                colCnt += curr;
             }
-            if(colCnt < m - colCnt){
-                for(int i = 0; i < m; i++){
-                    grid[i][j] ^= 1;
-                }
-            }
-        }
-
-        int ans = 0;
-        for(int i = 0; i < m; i++){            
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1){
-                    ans += (1 << (n - 1 - j));
-                }
-            }
+            colCnt = max(colCnt, m - colCnt);
+            ans += (colCnt * (1 << (n - 1 - j)));
         }
         return ans;
     }
