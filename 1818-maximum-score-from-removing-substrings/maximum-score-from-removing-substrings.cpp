34c321036f6ef0c007056aa3e346994852ce1a05
Awez Mirza <awezbaigmirza@gmail.com>
2024-07-12 23:51:02 +0530

    Time: 70 ms (79.68%) | Memory: 21.6 MB (71.95%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-
-    string removeStr(string s, string remove) {
+    string removeStr(string& s, string& remove) {
         stack<char> st;
 
-        for(auto ch : s) {
-            if(ch == remove[1] && !st.empty() && st.top() == remove[0]) {
+        for (auto ch : s) {
+            if (ch == remove[1] && !st.empty() && st.top() == remove[0]) {
                 st.pop();
             } else {
                 st.push(ch);
@@ -13,12 +12,30 @@ public:
         }
 
         string temp;
-        while(!st.empty()) {
+        while (!st.empty()) {
             temp.push_back(st.top());
             st.pop();
         }
         reverse(temp.begin(), temp.end());
         return temp;
+    }
+
+    string removeStrON(string& s, string& remove) {
+        int i = 0, j = 0;
+        int len = s.length();
+        while(j < len) {
+            s[i] = s[j];
+            i++;
+            if(i > 1 && s[i - 2] == remove[0] && s[i - 1] == remove[1]) {
+                i -= 2;
+            }            
+            j++;
+        }
+        while(i < len) {
+            s.pop_back();
+            i++;
+        } 
+        return s;
     }
 
     int maximumGain(string s, int x, int y) {
@@ -29,12 +46,12 @@ public:
         string minStr = x < y ? "ab" : "ba";
 
         // Max
-        string afterMax = removeStr(s, maxStr);
+        string afterMax = removeStrON(s, maxStr);
         int afterMaxLen = afterMax.length();
         ans = ((len - afterMaxLen) / 2) * max(x, y);
 
         // Min
-        string afterMin = removeStr(afterMax, minStr);
+        string afterMin = removeStrON(afterMax, minStr);
         ans += (((afterMaxLen - afterMin.length()) / 2) * min(x, y));
 
         return ans;
