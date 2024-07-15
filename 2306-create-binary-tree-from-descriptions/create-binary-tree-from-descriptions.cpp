cb336e42a9eedba88dd283560cd2faec46d9f6a6
Awez Mirza <awezbaigmirza@gmail.com>
2024-07-15 18:35:39 +0530

    Time: 795 ms (26.42%) | Memory: 298.9 MB (24.89%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -12,57 +12,31 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        map<int, TreeNode*> mp;
+        unordered_map<int, TreeNode*> mp;
         unordered_set<int> st;
+
         for (auto description : descriptions) {
             st.insert(description[1]);
-            if (mp.find(description[0]) != mp.end()) {
-                if (mp.find(description[1]) != mp.end()) {
-                    if (description[2] == 1) {
-                        mp[description[0]]->left = mp[description[1]];
-                    } else {
-                        mp[description[0]]->right = mp[description[1]];
-                    }
-                } else {
-                    TreeNode* nd = new TreeNode(description[1]);
-                    mp[description[1]] = nd;
-                    if (description[2] == 1) {
-                        mp[description[0]]->left = nd;
-                    } else {
-                        mp[description[0]]->right = nd;
-                    }
-                }
+
+            TreeNode* parent = mp.find(description[0]) != mp.end() ? mp[description[0]] : new TreeNode(description[0]);
+            TreeNode* child = mp.find(description[1]) != mp.end() ? mp[description[1]] : new TreeNode(description[1]);
+
+            if (description[2] == 1) {
+                parent->left = child;
             } else {
-                TreeNode* pr = new TreeNode(description[0]);
-                if (mp.find(description[1]) != mp.end()) {
-                    if (description[2] == 1) {
-                        pr->left = mp[description[1]];
-                    } else {
-                        pr->right = mp[description[1]];
-                    }
-                } else {
-                    TreeNode* nd = new TreeNode(description[1]);
-                    mp[description[1]] = nd;
-                    if (description[2] == 1) {
-                        pr->left = nd;
-                    } else {
-                        pr->right = nd;
-                    }
-                }
-                mp[description[0]] = pr;
+                parent->right = child;
             }
-            // mp[description[0]]->left 
+            
+            mp[description[0]] = parent;
+            mp[description[1]] = child;
         }
 
         for (auto description : descriptions) {
-            description[0];
             if (st.find(description[0]) == st.end()) {
                 return mp[description[0]];
             }
         }
 
-
-        TreeNode* root = new TreeNode(5);
-        return root;
+        return NULL;
     }
 };
