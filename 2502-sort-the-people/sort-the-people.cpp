91407ec4e0b87afee93faaa043c09e9cb4c61222
Awez Mirza <awezbaigmirza@gmail.com>
2024-07-22 12:38:33 +0530

    Time: 33 ms (40.77%) | Memory: 28.8 MB (22.51%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
 
-        priority_queue<pair<int, string>> pq;
+        priority_queue<pair<int, string>, vector<pair<int, string>>> pq;
 
         int n = names.size();
         for (int i = 0; i < n; i++) {
