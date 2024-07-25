d5b83831d61a4f16c8976bc88bc131003f3f744c
Awez Mirza <awezbaigmirza@gmail.com>
2024-07-25 19:48:39 +0530

    Time: 348 ms (38.40%) | Memory: 178.3 MB (28.80%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,58 +1,53 @@
 class Solution {
 public:
-    void merge(vector<int>& arr,int start,int end){
-        int mid = (start + end )>>1;
-        int len1 = mid-start+1;
-        int len2 = end-mid;
 
-        vector<int> left;
-        vector<int> right;
+    void merge(vector<int>& nums, int start, int end) {
+        int mid =  (start + end) / 2; 
 
-        int k = start;
-        for(int i = 0;i<len1;k++,i++){
-            left.push_back(arr[k]);
-        }
-        k = mid + 1;
-        for(int i = 0;i<len2;k++,i++){
-            right.push_back(arr[k]);
-        }
+        int len1 = mid - start + 1;
+        int len2 = end - mid;
+
+        vector<int> arr1(nums.begin() + start, nums.begin() + start + len1);
+        vector<int> arr2(nums.begin() + mid + 1, nums.begin() + end + 1);
+
         int ptr1 = 0;
         int ptr2 = 0;
-        while(ptr1<len1 && ptr2 < len2){
-            if(left[ptr1]<right[ptr2]){
-                arr[start] = left[ptr1];
-                start++,ptr1++;
+
+        while (ptr1 < len1 && ptr2 < len2) {
+            if (arr1[ptr1] < arr2[ptr2]) {
+                nums[start] = arr1[ptr1++];
+            } else {
+                nums[start] = arr2[ptr2++];
             }
-            else{
-                arr[start] = right[ptr2];
-                start++,ptr2++;
-            }
-        }
-        while(ptr1<len1){
-            arr[start] = left[ptr1];
-            start++,ptr1++;
-        }
-        while(ptr2<len2){
-            arr[start] = right[ptr2];
-            start++,ptr2++;
+            start++;
         }
 
+        while (ptr1 < len1) {
+            nums[start++] = arr1[ptr1++];
+        }
+
+        while (ptr2 < len2) {
+            nums[start++] = arr2[ptr2++];
+        }
     }
 
-    void mergeSort(vector<int>& arr,int start,int end){
-        if(start >= end ){
+    void mergeSort(vector<int>& nums, int start, int end) {
+        if (start >= end) {
             return;
         }
-        int mid = (start + end)>>1;
-        mergeSort(arr,start,mid);
-        mergeSort(arr,mid+1,end);
 
-        merge(arr,start,end);
+        int mid = (start + end) / 2; 
+
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid + 1, end);
+
+        merge(nums, start, end);
     }
 
     vector<int> sortArray(vector<int>& nums) {
-        int n = nums.size();
-        mergeSort(nums,0,n-1);
+        
+        mergeSort(nums, 0, nums.size() - 1);
+
         return nums;
     }
 };
