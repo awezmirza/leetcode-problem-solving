e868e314ee159c3341118216c8ecd6ca51b26da9
Awez Mirza <awezbaigmirza@gmail.com>
2024-08-01 23:25:17 +0530

    Time: 39 ms (25.94%) | Memory: 20.8 MB (30.95%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,79 +1,59 @@
 class Solution {
 public:
-    int countMaxAreaHistogram(vector<int>& histogram){
-        int n = histogram.size();
-        vector<int> prevLessIndex(n);
-        vector<int> nextLessIndex(n);
-
-        stack<int> st;
-        st.push(-1);
-
-        for (int i = 0; i < n; i++) {
-            while(st.top() != -1 && histogram[st.top()] >= histogram[i]) {
-                    st.pop();
-            }
-            prevLessIndex[i] = st.top();
-            st.push(i);
-        }
-
-        while (!st.empty()) {
-            st.pop();
-        }
-
-        st.push(n);
-
-        for (int i = n - 1; i >= 0; i--) {
-            while(st.top() != n && histogram[st.top()] >= histogram[i]) {
-                    st.pop();
-            }
-            nextLessIndex[i] = st.top();
-            st.push(i);
-        }
-
-        int ans = 0;
-        for (int i = 0; i < n; i++){
-            int curr = (nextLessIndex[i] - prevLessIndex[i] - 1) * histogram[i];
-            ans = max(ans, curr);
-        }
-
-        // cout<<"For histogram: "<<endl;
-        // for(auto el: histogram){
-        //     cout<<el<<" ";
-        // }
-        // cout<<endl;
-        // cout<<"prevLessIndex: "<<endl;
-        // for(auto el: prevLessIndex){
-        //     cout<<el<<" ";
-        // }
-        // cout<<endl;
-        // cout<<"nextLessIndex: "<<endl;
-        // for(auto el: nextLessIndex){
-        //     cout<<el<<" ";
-        // }
-        // cout<<endl;
-        return ans;
-    }
-
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        
-        vector<int> histogram(n, 0);
+        int rows = matrix.size();
+        int cols = matrix[0].size();
 
         int ans = 0;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '0') {
-                    histogram[j] = 0;
-                }
-                else {
-                    histogram[j] = histogram[j] + 1;
+        vector<vector<int>> intMatrix(rows, vector<int> (cols));
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                intMatrix[i][j] = matrix[i][j] == '0' ? 0 : 1;
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            if (i != 0) {
+                for (int j = 0; j < cols; j++) {                
+                    intMatrix[i][j] = (1 + intMatrix[i - 1][j]) * intMatrix[i][j];
                 }
             }
-            ans = max(ans, countMaxAreaHistogram(histogram));
-            // cout<<"After i = "<<i<<" ans is: "<<ans<<endl;
+
+            vector<int> prevLesser(cols);
+            stack<int> st;
+            st.push(-1);
+            for (int j = 0; j < cols; j++) {
+                while (st.top() != -1 && intMatrix[i][st.top()] >= intMatrix[i][j]) {
+                    st.pop();
+                }
+                prevLesser[j] = st.top();
+                st.push(j);
+            }
+
+            vector<int> nextLesserIndexes(cols);
+            stack<int> st2;
+            st2.push(cols);
+            for (int j = cols - 1; j >= 0; j--) {
+                while (st2.top() != cols && intMatrix[i][st2.top()] >= intMatrix[i][j]) {
+                    st2.pop();
+                }
+                nextLesserIndexes[j] = st2.top();
+                st2.push(j);
+            }
+
+            for (int j = 0; j < cols; j++) {
+                int nxtLesser = nextLesserIndexes[j];
+                int prvLesser = prevLesser[j];
+                int currHeight = intMatrix[i][j];
+
+                int currArea =( (j - prvLesser) + (nxtLesser - j) - 1) * currHeight;
+                ans = max(ans, currArea);
+
+            }
         }
+
         return ans;
     }
 };
