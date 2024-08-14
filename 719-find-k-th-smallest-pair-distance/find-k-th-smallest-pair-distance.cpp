188287723a82c84d4f42a21b3aeb355755285bfb
Awez Mirza <awezbaigmirza@gmail.com>
2024-08-14 21:05:04 +0530

    Time: 16 ms (36.91%) | Memory: 12.9 MB (44.69%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -1,22 +1,48 @@
 class Solution {
 public:
-    int smallestDistancePair(vector<int>& nums, int k) {
-        // priority_queue<int> pq;
+
+    int binSearch(vector<int>& nums, int i, int maxDist) {
+        int start = i + 1;
+        int end = nums.size() - 1;
+        int mid = (start + end) / 2;
+        while (start <= end) {
+            if (nums[mid] - nums[i] > maxDist) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            mid = (start + end) / 2;
+        }
+        return mid;
+    }
+
+    int solve(vector<int>& nums, int maxDist) {
+        int cnt = 0;
         int n = nums.size();
-        int maxEl = *max_element(nums.begin(), nums.end());
-        vector<int> allDists(maxEl + 1, 0);
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int dist = abs(nums[i] - nums[j]);
-                allDists[dist]++;
+            int j = binSearch(nums, i, maxDist);
+            cnt += j - i;
+        }
+        return cnt;
+    }
+
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int highest = nums.back() - nums[0];
+        int lowest = 0;
+        
+        int result = 0;
+        while (lowest <= highest) {
+            int mid = (lowest + highest) / 2;
+            int totalDistancesLessThanMid = solve(nums, mid);
+            if (totalDistancesLessThanMid < k) {
+                lowest = mid + 1;
+            } else {
+                result = mid;
+                highest = mid - 1;
             }
         }
-        for (int i = 0; i < maxEl + 1; i++) {
-            k -= allDists[i];
-            if (k <= 0) {
-                return i;
-            }
-        }
-        return 0;
+        
+        return result;
     }
 };
