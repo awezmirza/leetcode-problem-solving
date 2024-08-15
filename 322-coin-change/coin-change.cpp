329e2b71971a8d8b1430fef6927ef149b29a8c2a
Awez Mirza <awezbaigmirza@gmail.com>
2024-08-15 17:54:33 +0530

    Time: 22 ms (82.94%) | Memory: 16.5 MB (71.84%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -6,15 +6,16 @@ public:
             return 0;
         }
 
+        if (amount < 0) {
+            return INT_MAX;
+        }
+
         if (dp[amount] != -1) {
             return dp[amount];
         }
         int n = coins.size();
         int mini = INT_MAX;
-        for (int i = n - 1; i >= 0; i--) {
-            if (amount - coins[i] < 0) {
-                continue;
-            }
+        for (int i = 0; i < n; i++) {
             int returned = solve(coins, amount - coins[i], dp);
             if (returned != INT_MAX) {
                 mini = min(mini, returned + 1);
@@ -23,10 +24,29 @@ public:
         return dp[amount] = mini;
     }
 
+    int solveSO(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, 0);
+        int n = coins.size();
+        for (int j = 1; j <= amount; j++) {
+            int mini = INT_MAX;
+
+            for (int i = n - 1; i >= 0; i--) {
+                if (j - coins[i] < 0) {
+                    continue;
+                }
+                int returned = dp[j - coins[i]];
+                if (returned != INT_MAX) {
+                    mini = min(mini, returned + 1);
+                }
+            }
+            dp[j] = mini;
+        }
+        return dp[amount];
+    }
+
     int coinChange(vector<int>& coins, int amount) {
         sort(coins.begin(), coins.end());
-        vector<int> dp(amount + 1, -1);
-        int ans = solve(coins, amount, dp);
+        int ans = solveSO(coins, amount);
         return ans == INT_MAX ? -1 : ans;
     }
 };
