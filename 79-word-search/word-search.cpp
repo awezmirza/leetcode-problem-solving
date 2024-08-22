edc45e903d4eb24f69885f968d51e0f5b18615dd
Awez Mirza <awezbaigmirza@gmail.com>
2024-08-22 17:07:02 +0530

    Time: 349 ms (68.96%) | Memory: 11.9 MB (33.84%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,57 +1,66 @@
 class Solution {
 public:
-    bool isPossible(int i, int j, vector<vector<char>>& board, string word,
-                    int index, vector<vector<bool>>& isVisited) {
-        if (index == word.size() - 1) {
-            return board[i][j] == word[index];
+    bool findAns(vector<vector<char>>& board, string word, int i, int j, int idx) {
+        if (idx >= word.size()) {
+            return true;
         }
-        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size()) {
-            return false;
+        int m = board.size();
+        int n = board[0].size();
+
+        // Down
+        if (i < m - 1 && board[i + 1][j] == word[idx]) {
+            char temp = board[i + 1][j];
+            board[i + 1][j] = '_';
+            if (findAns(board, word, i + 1, j, idx + 1)) {
+                return true;
+            }
+            board[i + 1][j] = temp;
         }
-        bool ans = 0;
-        if (board[i][j] == word[index]) {
-            // Left
-            if (i > 0 && !isVisited[i - 1][j]) {
-                isVisited[i][j] = true;
-                ans = ans ||
-                      isPossible(i - 1, j, board, word, index + 1, isVisited);
-                isVisited[i][j] = false;
+
+        // Left
+        if (j > 0 && board[i][j - 1] == word[idx]) {
+            char temp = board[i][j - 1];
+            board[i][j - 1] = '_';
+            if (findAns(board, word, i, j - 1, idx + 1)) {
+                return true;
             }
-            // Right
-            if (i < board.size() - 1 && !isVisited[i + 1][j]) {
-                isVisited[i][j] = true;
-                ans = ans ||
-                      isPossible(i + 1, j, board, word, index + 1, isVisited);
-                isVisited[i][j] = false;
-            }
-            // Up
-            if (j > 0 && !isVisited[i][j - 1]) {
-                isVisited[i][j] = true;
-                ans = ans ||
-                      isPossible(i, j - 1, board, word, index + 1, isVisited);
-                isVisited[i][j] = false;
-            }
-            // Down
-            if (j < board[0].size() - 1 && !isVisited[i][j + 1]) {
-                isVisited[i][j] = true;
-                ans = ans ||
-                      isPossible(i, j + 1, board, word, index + 1, isVisited);
-                isVisited[i][j] = false;
-            }
+            board[i][j - 1] = temp;
         }
-        return ans;
+
+        // Right
+        if (j < n - 1 && board[i][j + 1] == word[idx]) {
+            char temp = board[i][j + 1];
+            board[i][j + 1] = '_';
+            if (findAns(board, word, i, j + 1, idx + 1)) {
+                return true;
+            }
+            board[i][j + 1] = temp;
+        }
+
+        // Up
+        if (i > 0 && board[i - 1][j] == word[idx]) {
+            char temp = board[i - 1][j];
+            board[i - 1][j] = '_';
+            if (findAns(board, word, i - 1, j, idx + 1)) {
+                return true;
+            }
+            board[i - 1][j] = temp;
+        }
+        return false;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size();
         int n = board[0].size();
-
-        vector<vector<bool>> isVisited(m, vector<bool>(n, 0));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                bool ans = isPossible(i, j, board, word, 0, isVisited);
-                if (ans) {
-                    return true;
+                if (board[i][j] == word[0]) {
+                    char temp = board[i][j];
+                    board[i][j] = '_';
+                    if (findAns(board, word, i, j, 1)) {
+                        return true;
+                    }
+                    board[i][j] = temp;
                 }
             }
         }
