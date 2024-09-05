e0d5318d9d5b9a84d6d0aec07fede8157691bfb9
Awez Mirza <awezbaigmirza@gmail.com>
2024-09-05 21:59:26 +0530

    Time: 92 ms (76.72%) | Memory: 116.3 MB (86.03%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.cpp
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+
+        // Calculating sum of all values of rolls
         int sumOfAllNums = accumulate(rolls.begin(), rolls.end(), 0);
+        
         int m = rolls.size();
+
+        // Calculating sum of ans vector
         int ansVecKaSum = ((mean * (n + m)) - sumOfAllNums);
+
+        // Checking for the not possible values
         if (n > ansVecKaSum || 6 * n < ansVecKaSum) {
             return {};
         }
+
+        // Calculating the number that will be filled in all ans vector
         int toFill = ansVecKaSum / n;
+
+        // Getting the remaining sum after filling the vector with the above value
         int remain = ansVecKaSum % n;
+
+        // Creating and filling ans vector
         vector<int> ans(n, toFill);
+
+        // Filling the remaining value greedily
         for (int i = 0; i < remain; i++) {
             ans[i]++;
         }
+        
         return ans;
     }
 };
