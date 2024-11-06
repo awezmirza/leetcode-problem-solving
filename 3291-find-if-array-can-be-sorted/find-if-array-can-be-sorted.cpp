f73ccf955b32b3ce9f2d69b110b565c6b4ca6e9f
Awez Mirza <awezbaigmirza@gmail.com>
2024-11-06 18:26:06 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (63.03%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ppcnt;
-        for (int num : nums) {
-            ppcnt.push_back(__builtin_popcount(num));
-        }
-        int prevMaxi = -1;
-        int currMaxi = INT_MIN;
-        int currMin = INT_MAX;
-        int i = 0;
+        int currMaxi = INT_MIN, currMin = INT_MAX, prevMaxi = -1;
+        int i = 0, n = nums.size();
 
         while (i < n) {
             int currppCnt = __builtin_popcount(nums[i]);
