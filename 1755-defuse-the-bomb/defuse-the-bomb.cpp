436eab2783096b3fa0a16d1d146888fa06c3a416
Awez Mirza <awezbaigmirza@gmail.com>
2024-11-19 00:10:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (84.00%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -3,25 +3,39 @@ public:
     vector<int> decrypt(vector<int>& code, int k) {
         int n = code.size();
         vector<int> ans(n, 0);
+        
+        
         if (k > 0) {
+            int cumSum = 0;
+
+            for (int i = 0; i < k; i++) {
+                cumSum += code[(i + 1) % n];
+            }
+            
             for (int i = 0; i < n; i++) {
-                int sum = 0;
-                for (int j = 1; j <= k; j++) {
-                    sum += code[(j + i) % n];
-                }
-                ans[i] = sum;
+                ans[i] = cumSum;
+                cumSum -= code[(i + 1) % n];
+                cumSum += code[(i + 1 + k) % n];
             }
         } else if (k < 0) {
-            for (int i = 0; i < n; i++) {
-                int sum = 0;
-                for (int j = 1; j <= -(k); j++) {
-                    int idx = i - j;
-                    if (idx < 0) {
-                        idx += n;
-                    }
-                    sum += code[idx];
+            int cumSum = 0;
+
+            for (int i = 1; i <= -(k); i++) {
+                int idx = -(i);
+                if (idx < 0) {
+                    idx += n;
                 }
-                ans[i] = sum;
+                cumSum += code[idx];
+            }
+            
+            for (int i = 0; i < n; i++) {
+                ans[i] = cumSum;
+                cumSum += code[i];
+                int subIdx = i + k;
+                if (subIdx < 0) {
+                    subIdx += n;
+                }
+                cumSum -= code[subIdx];
             }
         }
         return ans;
