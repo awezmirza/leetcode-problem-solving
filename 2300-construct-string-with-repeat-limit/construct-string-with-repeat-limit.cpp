406ac985891bb8d01aa494979f61de210236ae94
Awez Mirza <awezbaigmirza@gmail.com>
2024-12-17 23:34:25 +0530

    Time: 5 ms (96.71%) | Memory: 27.8 MB (49.26%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     string repeatLimitedString(string s, int repeatLimit) {
+
         vector<int> freq(26, 0);
         for (char c : s) {
             freq[c - 'a']++;
         }
+
         string ans;
         bool used = true;
         bool repeated = false;
         int ptr = 25;
+
         while (used && ptr >=0) {
-            used = false;
-            if (freq[ptr] == 0) {
-                ptr--;
-                used = true;
-            } else {
-                if (!repeated) {
-                    int cycle = min(repeatLimit, freq[ptr]);
-                    for (int i = 0; i < cycle; i++) {
-                        ans.push_back(ptr + 'a');
-                    }
-                    if (cycle != freq[ptr]) {
-                        repeated = true;
-                    }
-                    freq[ptr] -= cycle;
-                    used = true;
+            if (!repeated) {
+                int cycle = min(repeatLimit, freq[ptr]);
+                for (int i = 0; i < cycle; i++) {
+                    ans.push_back(ptr + 'a');
+                }
+                freq[ptr] -= cycle;
+                if (freq[ptr] != 0) {
+                    repeated = true;
                 } else {
-                    int toFind = ptr - 1;
-                    while (toFind >= 0 && !used) {
-                        if (freq[toFind] > 0) {
-                            ans.push_back(toFind + 'a');
-                            freq[toFind]--;
-                            used = true;
-                            repeated = false;
-                        }
-                        toFind--;
+                    ptr--;
+                }
+            } else {
+                int toFind = ptr - 1;
+                used = false;
+                while (toFind >= 0 && !used) {
+                    if (freq[toFind] > 0) {
+                        ans.push_back(toFind + 'a');
+                        freq[toFind]--;
+                        used = true;
+                        repeated = false;
                     }
+                    toFind--;
                 }
             }
         }
