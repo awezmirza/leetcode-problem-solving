7ad000543439d9eb90c3fc89f5a8f548ddfd2ffe
Awez Mirza <awezbaigmirza@gmail.com>
2024-12-18 23:03:31 +0530

    Time: 1 ms (23.39%) | Memory: 14.2 MB (17.75%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -4,9 +4,7 @@ public:
         int n = prices.size();
         vector<int> ans(n);
         stack<int> st;
-        st.push(n - 1);
-        ans[n - 1] = prices[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             while (!st.empty() && prices[st.top()] > prices[i]) {
                 st.pop();
             }
