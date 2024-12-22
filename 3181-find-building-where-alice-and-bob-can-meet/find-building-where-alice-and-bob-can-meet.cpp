03556862b20378975fd1c924bfef487699b17e66
Awez Mirza <awezbaigmirza@gmail.com>
2024-12-22 22:14:52 +0530

    Time: 246 ms (59.87%) | Memory: 293.9 MB (38.86%) - LeetSync

--- 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
+++ 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
@@ -1,9 +1,11 @@
+#define P pair<int, int>
+
 class Solution {
 public:
     vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
         int n = queries.size();
         vector<int> ans(n, -1);
-        unordered_map<int, vector<pair<int, int>>> mp;
+        unordered_map<int, vector<P>> mp;
         for (int i = 0; i < n; i++) {
             auto query = queries[i];
             sort(query.begin(), query.end());
@@ -13,7 +15,7 @@ public:
                 mp[query[1]].push_back({heights[query[0]], i});
             }
         }
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        priority_queue<P, vector<P>, greater<P>> pq;
         int k = heights.size();
         for (int i = 0; i < k; i++) {
             if (mp.count(i)) {
