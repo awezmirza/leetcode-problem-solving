8704abc4365e83954813dde4eb6156162b985ec9
Awez Mirza <awezbaigmirza@gmail.com>
2025-01-04 14:01:20 +0530

    Time: 143 ms (85.58%) | Memory: 16 MB (56.14%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -4,24 +4,14 @@ public:
         vector<int> first(26, -1);
         vector<int> last(26, -1);
         int n = s.size();
-        vector<int> unique(n, 0);
-        unique[0] = 1;
 
         for (int i = 0; i < n; i++) {
-            char c = s[i];
+            int idx = s[i] - 'a';
 
-            if (i != 0) {
-                if (first[c - 'a'] == -1) {
-                    unique[i] = unique[i - 1] + 1;
-                } else {
-                    unique[i] = unique[i - 1];
-                }
-            }
-
-            if (first[c - 'a'] == -1) {
-                first[c - 'a'] = i;
+            if (first[idx] == -1) {
+                first[idx] = i;
             } else {
-                last[c - 'a'] = i;
+                last[idx] = i;
             }
         }
 
