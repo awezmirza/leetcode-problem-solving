a91298dc3295bf7ddd6862a4cebb79e23be15c6e
Awez Mirza <awezbaigmirza@gmail.com>
2025-01-07 00:30:39 +0530

    Time: 4 ms (57.50%) | Memory: 13 MB (6.09%) - LeetSync

---
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n = boxes.size();
+
+        vector<pair<int, int>> left(n);
+        // left[0].second = boxes[0] - '0';
+        for (int i = 1; i < n; i++) {
+            left[i].second = boxes[i - 1] - '0' + left[i - 1].second;
+            int pre = left[i - 1].second;
+            left[i].first = left[i - 1].first + pre + boxes[i - 1] - '0';
+        }
+
+        vector<pair<int, int>> right(n);
+        // right[n - 1].second = boxes[n - 1] - '0';
+        for (int i = n - 2; i >= 0; i--) {
+            right[i].second = boxes[i + 1] - '0' + right[i + 1].second;
+            int pre = right[i + 1].second;
+            right[i].first = right[i + 1].first + pre + boxes[i + 1] - '0';
+        }
+
+        vector<int> ans(n, 0);
+        for (int i = 0; i < n; i++) {
+            ans[i] = left[i].first + right[i].first;
+        }
+
+        return ans;
+    }
+};
