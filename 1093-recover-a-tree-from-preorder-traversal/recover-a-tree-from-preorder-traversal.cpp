226d5f3a8b511891ca31b9190e6d2d56cdaaa0ea
Awez Mirza <awezbaigmirza@gmail.com>
2025-02-22 23:29:21 +0530

    Time: 3 ms (62.80%) | Memory: 20.9 MB (72.65%) - LeetSync

--- 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
@@ -11,6 +11,44 @@
  */
 class Solution {
 
+    int takeNum(string& traversal, int& i) {
+        int val = 0;
+
+        while (i < traversal.size() && traversal[i] != '-') {
+            val = val * 10 + (traversal[i] - '0');
+            i++;
+        }
+
+        return val;
+    }
+
+    int countDashes(string& traversal, int& i) {
+        int dashes = 0;
+        while (traversal[i] == '-') {
+            dashes++;
+            i++;
+        }
+        return dashes;
+    }
+
+    void emptyStack(int& currLvl, int& dashes, stack<TreeNode*>& st) {
+        while (currLvl != dashes - 1) {
+            currLvl--;
+            st.pop();
+        }
+    }
+
+    void insertInTreeAndStack(stack<TreeNode*>& st, int& val) {
+        TreeNode* tp = st.top();
+        if (!tp->left) {
+            tp->left = new TreeNode(val);
+            st.push(tp->left);
+        } else {
+            tp->right = new TreeNode(val);
+            st.push(tp->right);
+        }
+    }
+
 public:
     TreeNode* recoverFromPreorder(string traversal) {
         TreeNode* root = new TreeNode();
@@ -18,42 +56,18 @@ public:
         stack<TreeNode*> st;
         st.push(root);
 
-        int currLvl = -1;
-        int dashes = 0;
-        int i = 0;
+        int currLvl = -1, dashes = 0, i = 0;
 
         while (i < traversal.size()) {
             if (traversal[i] == '-') {
-                dashes = 0;
-                while (traversal[i] == '-') {
-                    dashes++;
-                    i++;
-                }
+                dashes = countDashes(traversal, i);
             } else {
-
-                int val = 0;
-                while (i < traversal.size() && traversal[i] != '-') {
-                    val = val * 10 + (traversal[i] - '0');
-                    i++;
-                }
-
-                while (currLvl != dashes - 1) {
-                    currLvl--;
-                    st.pop();
-                }
-
-                TreeNode* tp = st.top();
-                if (!tp->left) {
-                    tp->left = new TreeNode(val);
-                    st.push(tp->left);
-                } else {
-                    tp->right = new TreeNode(val);
-                    st.push(tp->right);
-                }
+                int val = takeNum(traversal, i);
+                emptyStack(currLvl, dashes, st);
+                insertInTreeAndStack(st, val);                
                 currLvl++;
             }
         }
-
 
         return root->left;
     }
