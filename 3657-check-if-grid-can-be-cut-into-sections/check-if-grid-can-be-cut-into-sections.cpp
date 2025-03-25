f015bfb573e849a5179442364c664a97dcd1580f
Awez Mirza <awezbaigmirza@gmail.com>
2025-03-26 02:11:03 +0530

    Time: 707 ms (6.96%) | Memory: 315.1 MB (10.09%) - LeetSync

---
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.cpp
@@ -0,0 +1,43 @@
+class Solution {
+
+    bool isPossible(vector<vector<int>>& range, int& n) {
+        sort(range.begin(), range.end());
+        vector<vector<int>> newRange;
+
+        int st = range[0][0];
+        int en = range[0][1];
+        int sz = range.size();
+
+        for (int i = 1; i < sz; i++) {
+            if (en <= range[i][0]) {
+                newRange.push_back({st, en});
+                st = range[i][0];
+                en = range[i][1];
+            } else {
+                en = max(en, range[i][1]);
+            }
+        }
+        newRange.push_back({st, en});
+
+        for (auto rng : newRange) {
+            cout<<rng[0]<<", "<<rng[1]<<" -- ";
+        }
+        cout<<endl;
+
+        // cout<<newRange.size()<<" ";
+
+        return newRange.size() >= 3;
+    }
+
+public:
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        vector<vector<int>> x, y;
+
+        for (auto rect : rectangles) {
+            x.push_back({rect[0], rect[2]});
+            y.push_back({rect[1], rect[3]});
+        }
+
+        return isPossible(x, n) || isPossible(y, n);
+    }
+};
