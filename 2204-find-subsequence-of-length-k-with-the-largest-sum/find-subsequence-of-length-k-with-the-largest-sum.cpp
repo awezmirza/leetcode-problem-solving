e558d84fe7ad3303bd04ef6c7b13178b5be62f22
Awez Mirza <awezbaigmirza@gmail.com>
2025-06-28 23:19:49 +0530

    Time: 7 ms (7.68%) | Memory: 14.1 MB (22.93%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,38 +1,33 @@
 class Solution {
 
-    static bool comp(pair<int, int> a, pair<int, int> b) {
+    auto static comp(pair<int, int> a, pair<int, int> b) {
         return a.second < b.second;
     }
 
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<pair<int, int>> cp;
         int n = nums.size();
         for (int i = 0; i < n; i++) {
-            if (pq.size() < k) {
-                pq.push({nums[i], i});
-            } else {
-                if (pq.top().first < nums[i]) {
-                    pq.pop();
-                    pq.push({nums[i], i});
-                }
-            }
+            cp.push_back({nums[i], i});
         }
 
-        vector<pair<int, int>> ans;
-        while (!pq.empty()) {
-            ans.push_back(pq.top());
-            pq.pop();
+        sort(cp.begin(), cp.end());
+
+        vector<pair<int, int>> cp2;
+
+        for (int i = n - k; i < n; i++) {
+            cp2.push_back(cp[i]);
         }
 
-        sort(ans.begin(), ans.end(), comp);
+        sort(cp2.begin(), cp2.end(), comp);
 
-        vector<int> res;
+        vector<int> ans;
 
         for (int i = 0; i < k; i++) {
-            res.push_back(ans[i].first);
+            ans.push_back(cp2[i].first);
         }
 
-        return res;
+        return ans;
     }
 };
