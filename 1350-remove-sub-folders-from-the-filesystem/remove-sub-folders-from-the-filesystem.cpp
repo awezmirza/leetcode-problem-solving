7aa77cb71f8a885dbf3f16dec8dfbff55c452fcb
Awez Mirza <awezbaigmirza@gmail.com>
2025-07-19 23:43:40 +0530

    Time: 62 ms (60.21%) | Memory: 50 MB (66.24%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        // Sort the folders lexicographically so parent folders come before their subfolders
         sort(folder.begin(), folder.end());
+        unordered_set<string> st;
 
-        // Initialize result vector with the first folder
-        vector<string> ans;
-        ans.push_back(folder[0]);
+        for (auto& path : folder) {
+            string curr = "/";
 
-        // Iterate through remaining folders starting from index 1
-        for(int i = 1; i < folder.size(); i++) {
-            // Get the last added folder path and add a trailing slash
-            // This helps in comparing if current folder is a subfolder
-            string lastFolder = ans.back();
-            lastFolder.push_back('/');
+            int i = 1;
+            bool flag = false;
+            while (i < path.size()) {
 
-            // Compare current folder with last added folder
-            // compare(0, lastFolder.size(), lastFolder) checks if folder[i] starts with lastFolder
-            // If it doesn't start with lastFolder (returns != 0), then it's not a subfolder
-            if(folder[i].compare(0, lastFolder.size(), lastFolder) != 0) {
-                // If not a subfolder, add to result
-                ans.push_back(folder[i]);
+                if (flag) {
+                    break;
+                }
+                
+                while (i < path.size() && path[i] != '/') {
+                    if (flag) {
+                        break;
+                    }
+                    curr.push_back(path[i]);
+                    i++;
+                }
+
+                if (st.contains(curr)) {
+                    flag = true;
+                }
+
+                curr.push_back('/');
+
+                i++;
             }
+
+            if (!flag) {
+                st.insert(path);
+            }
+        }
+
+        vector<string> ans;
+
+        for (auto &str : st) {
+            ans.push_back(str);
         }
 
         return ans;
