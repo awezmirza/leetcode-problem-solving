1c0ed2da253874ef42a551702c54d0b2bf5c2f8d
Awez Mirza <awezbaigmirza@gmail.com>
2025-08-14 15:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (77.37%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
+        int ans = -1;
         int n = num.size();
-        int large = 0;
-        string ans = "";
-        for(int i=0;i<n-2;i++){
-            if(num[i] > large && num[i] == num[i + 1] && num[i] == num[i+2]){
-                ans = "";
-                large = num[i];
-                ans.push_back(num[i]);
-                ans.push_back(num[i]);
-                ans.push_back(num[i]);
-                i += 2;
+        for (int i = 0; i < n - 2; i++) {
+            if (num[i] == num[i + 1] && num[i] == num[i + 2]) {
+                ans = max(ans, num[i] - '0');
             }
         }
-        return ans;
+
+        if (ans == -1) {
+            return "";
+        }
+
+        char an = ans + '0';
+
+        string a = "";
+        a += an;
+        a += an;
+        a += an;
+
+        return a;
     }
 };
